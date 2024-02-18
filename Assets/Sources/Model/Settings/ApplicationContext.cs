45b7354a2de979ab2e3dcef7c537acf718c3e9c3
dpr3309 <dpr3309@gmail.com>
2024-02-18 22:55:06 +0300

    application settings edited

--- Assets/Sources/Model/Settings/ApplicationContext.cs
+++ Assets/Sources/Model/Settings/ApplicationContext.cs
@@ -7,7 +7,7 @@ namespace Clicker.Model
         private const int MIN_CRYSTALS_COUT = 8;
         private const float RELEASE_OBJECTS_OFFSET = 2.5f;
         private const int MIN_TILES_COUNT = 30;
-        private const float PLAYER_CHIP_SPEED = 0.05f;
+        private const float PLAYER_CHIP_SPEED = 0.095f;
         private readonly Vector3 START_POSTITION = new (0, 0.7f, 0);
 
         internal int MinCrystalsCount => MIN_CRYSTALS_COUT;
